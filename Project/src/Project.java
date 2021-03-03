d654663a8f546a1ec4d17bb99684e73fe185e7bf
Justin Alexander Vasconez Chasi <75390354+JustinAlex26@users.noreply.github.com>
2021-03-02 19:43:29 -0500

    Update Project.java

--- Project/src/Project.java
+++ Project/src/Project.java
@@ -13,6 +13,46 @@ public class Project {
     public static void main(String[] args) {
         // TODO code application logic here
 
+        case 2:
+
+                    double Sx1;
+                    double Sx2;
+                    double Sy1;
+                    double Sy2;
+                    double disites;
+                    System.out.println(" <-- Insert a Sx1 -->");
+                    Sx1 = input.nextInt();
+
+                    System.out.println(" <-- Insert a Sx2 -->");
+                    Sx2 = input.nextInt();
+
+                    System.out.println(" <-- Insert a Sy1 -->");
+                    Sy1 = input.nextInt();
+
+                    System.out.println(" <-- Insert a Sy2 -->");
+                    Sy2 = input.nextInt();
+
+                    disites = performTheDistanceBetweenTwoPlaces(Sx1, Sx2, Sy1, Sy2);
+                    System.out.println(" <- disites is -> " + disites);
+                    break;
+
+                case 3:
+
+                    int Currentvariable = 0;
+                    int Oldvariable = 0;
+                    final int valor = 100;
+                    float y = 0;
+                    y = calculateThePopulationPercent(Currentvariable, Oldvariable, valor);
+                    System.out.println("The percentage is");
+                    System.out.println("enter Currentvariable -> ");
+                    Currentvariable = input.nextInt();
+                    System.out.println("enter Oldvariable -> ");
+                    Oldvariable = input.nextInt();
+                    y = ((Currentvariable - Oldvariable) / Oldvariable) * valor;
+
+                    y = calculateThePopulationPercent(Currentvariable, Oldvariable, valor);
+                    System.out.println(" percentage of" + y + "is equal to ->" + y);
+                    break;
                 case 4:
 
                     int mainRivers;
