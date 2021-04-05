595a3fa23612ac6d8f20ddd8c003a92108ae4146
Alessiovillalba <75390436+Alessiovillalba@users.noreply.github.com>
2021-04-05 12:11:01 -0500

    Final Project

---
+++ Proyecto/src/proyecto/provincias/y/capitales/PROVINCIASYCAPITALES.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provincias.y.capitales;
+
+/**
+ *
+ * @author User
+ */
+public class PROVINCIASYCAPITALES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //EMPIEZO A HACER EL CODIGO
+    }
+    
+}
